1308bb7ac3e582edc2f3af23f6b04a832e0dea00
Steve <sagarvedant01@gmail.com>
2025-05-03 03:13:40 -0500

    add home

--- back-end/main.py
+++ back-end/main.py
@@ -9,7 +9,7 @@ templates = Jinja2Templates(directory="templates")
 
 app.mount(path="/back-end/static", app=StaticFiles(directory="back-end/static"), name="static")
 
-@app.get(path="/")
+@app.get(path="/", response_class=HTMLResponse)
 def home(request: Request) -> HTMLResponse:
 	response = templates.TemplateResponse("home.html", {"request": request})
 	return HTMLResponse(content=response.body)
